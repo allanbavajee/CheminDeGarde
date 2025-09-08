da0c87da13ebfb2cbee881a8cd54e196ad67e3a5
allanbavajee <allan.bavajee@gmail.com>
2025-09-08 12:36:33 +0400

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Utiliser les variables d'environnement de Vercel ou .env.local
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-// Création du client Supabase
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
