856c3e705487c095ff67f2bacef46ca6e57bee81
allanbavajee <allan.bavajee@gmail.com>
2025-09-18 14:00:19 +0400

    Update page.tsx

--- app/admin/technique/page.tsx
+++ app/admin/technique/page.tsx
@@ -24,7 +24,7 @@ export default function IntercessionPage() {
 
   return (
     <div className="p-6 max-w-3xl mx-auto">
-      <h1 className="text-2xl font-bold mb-4">Rapport - Intercession</h1>
+      <h1 className="text-2xl font-bold mb-4">Rapport - Comm/Technique</h1>
 
       <div className="space-y-4">
         <input
